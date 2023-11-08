dd6ab1fae80069bd2b00870cda8681d7b7161848
Shravana9535 <131895234+Shravana9535@users.noreply.github.com>
2023-11-08 15:20:24 +0530

    Add files via upload

---
+++ Scenario2.java
@@ -0,0 +1,40 @@
+package Qsp;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Scenario2 {
+
+	public static void main(String[] args) {
+  WebDriver driver=new ChromeDriver();
+  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+  driver.get("https://testsheepnz.github.io/BasicCalculator.html");
+  driver.findElement(By.id("selectBuild")).click();
+  WebElement firstNumber=driver.findElement(By.id("number1Field"));
+  WebElement secondNumber=driver.findElement(By.id("number2Field"));
+  firstNumber.sendKeys("12");
+  secondNumber.sendKeys("34");
+  Select operationDropdown=new Select(driver.findElement(By.id("selectOperationDropdown")));
+  operationDropdown.selectByVisibleText("Multiply");
+  
+ driver.findElement(By.id("calculateButton")).click();
+  String result=driver.findElement(By.id("numberAnswerField")).getText();
+  String expectedResult="408";
+  if(result.equals(expectedResult))
+  {
+	  System.out.println("pass");
+  }
+  else
+  {
+	  System.out.println("fail");
+  }
+  driver.quit();
+  
+	}
+
+}
